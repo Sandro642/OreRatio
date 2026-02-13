5177983cbd2b025989f9e9b7ba9bf846f9aa37fe
Sandro <sandro33810@gmail.com>
2026-02-13 14:39:20 +0100

    initialize project structure with Gradle setup and essential configuration files

---
+++ src/main/java/fr/sandro642/github/OreRatio.java
@@ -0,0 +1,16 @@
+package fr.sandro642.github;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class OreRatio extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+}
